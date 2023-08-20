f7f640b7b7e1f14e3582e30b5268dd0fcb5bbc70
Sameer <130269325+SameerDev3023@users.noreply.github.com>
2023-08-20 10:13:44 +0530

    Add files via upload
    This file contain all pattern maker function code

---
+++ practise-01.js
@@ -0,0 +1,252 @@
+let PatternMaker = (patterName, patternShape,Row=5,Col=5)=>{
+    let patternString = '';
+if(patterName=='star'){
+    if(patternShape=='square' || patternShape=='default'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=Col;j++){
+                patternString += '*';
+            }
+            patternString += '\n';
+        }
+    }else if(patternShape=='right-pyramid'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=i;j++){
+                patternString += '*';
+            }
+            patternString += '\n';
+        }
+    }else if(patternShape=='reverse-right-pyramid'){
+        for(let i = Row ; i>=1;i--){
+            for(let j = i ; j>=1;j--){
+                patternString += '*';
+            }
+            patternString += '\n';
+        }
+    }else if(patternShape=='left-pyramid'){
+        for (let i = 1; i <= Row; i++) {   
+            for (let space = 1; space <= Row - i; space++) {
+                patternString += ' ';
+            }
+            
+            for (let j = 1; j <= i; j++) {
+                patternString += '*';
+            }
+            patternString += '\n';
+        }
+    }else if(patternShape =='pyramid'){
+        for (let i = 1; i <= Row; i++) {   
+            for (let space = 1; space <= Row - i; space++) {
+                patternString += ' ';
+            }
+            for (let j = 1; j <= i; j++) {
+                patternString += ' *';
+            }
+            patternString += '\n';
+        }
+    }
+    else if(patternShape =='octagon-square'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=Col;j++){
+                if (i === 1 || i === Row || j === 1 || j === Col || i==j || i-1==Col-j ||  j%7==0 || i%7==0) {
+                    patternString += ` *`;
+                } else {
+                    patternString += '  ';
+                }
+            }
+            patternString += '\n';
+        }
+    }else if(patternShape =='cross'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=Col;j++){
+                if (i==j || i-1==Col-j ) {
+                    patternString += '* ';
+                } else {
+                    patternString += '  ';
+                }
+            }
+            patternString += '\n';
+        }
+    } 
+    else if(patternShape =='arrow'){
+        for (let i = 1; i <= Row; i++) {
+            let patternString = '';
+           
+            for (let j = 1; j <= Col; j++) {
+                
+                if (i % 7 == 0 || j % 7 == 0 ||i<=7 && j==8-i||i<=7 && j==6+i) {
+                    patternString += ' *';
+                } else {
+                    patternString += '  ';
+                }
+            }
+    
+            patternString += '\n';
+            console.log(patternString);
+        }
+    } else if(patternShape =='kite'){
+        for (let i = 1; i <= Row; i++) {
+            let patternString = '';
+           
+            for (let j = 1; j <= Col; j++) {
+                
+                if (i % 7 == 0 || j % 7 == 0 ||i<=7 && j==8-i||i<=7 && j==6+i||  i>7 &&j==i-6 || i>7 && j==20-i ) {
+                    patternString += ' *';
+                } else {
+                    patternString += '  ';
+                }
+            }
+    
+            patternString += '\n';
+            console.log(patternString);
+        }
+    }
+    else if(patternShape =='empty-square-rhombus'){
+        for (let i = 1; i <= Row; i++) {
+            let patternString = '';
+           
+            for (let j = 1; j <= Col; j++) {
+                
+                if (i==1||j==1||i==Row||j==Col||i<=7 && j==8-i||i<=7 && j==6+i||  i>7 &&j==i-6 || i>7 && j==20-i ) {
+                    patternString += ' *';
+                } else {
+                    patternString += '  ';
+                }
+            }
+    
+            patternString += '\n';
+            console.log(patternString);
+        }
+    }else if(patternShape =='empty-rhombus'){
+        for (let i = 1; i <= Row; i++) {
+            let patternString = '';
+           
+            for (let j = 1; j <= Col; j++) {
+                
+                if (i<=7 && j==8-i||i<=7 && j==6+i||  i>7 &&j==i-6 || i>7 && j==20-i ) {
+                    patternString += ' *';
+                } else {
+                    patternString += '  ';
+                }
+            }
+    
+            patternString += '\n';
+            console.log(patternString);
+        }
+    }
+    else if(patternShape =='rhombus-inside-empty'){
+        for (let i = 1; i <= Row; i++) {
+            for (let j = 1; j <= Col; j++) {     
+                if(j<=7 && i<=7-j || j>7 && i<=j-7 || i>=7 && j<=i-7 ||i>=7 && j>=21-i){
+                    patternString += ' *';
+                }else{
+                    patternString += '  ';
+                }
+            }
+            patternString += '\n';
+        }
+    }
+    else if(patternShape =='rhombus'){
+        for (let i = 1; i <= Row; i++) {
+            for (let j = 1; j <= Col; j++) {     
+                if(j<=7 && i<=7-j || j>7 && i<=j-7 || i>=7 && j<=i-7 ||i>=7 && j>=21-i){
+                    patternString += '  ';
+                }else{
+                    patternString += ' *';
+                }
+            }
+            patternString += '\n';
+        }
+    }
+    else if(patternShape =='empty-square'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=Col;j++){
+                if (i === 1 || i === Row || j === 1 || j === Col) {
+                    patternString += '* ';
+                } else {
+                    patternString += '  ';
+                }
+            }
+            patternString += '\n';
+        }
+    } else if(patternShape =='cross-empty-square'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=Col;j++){
+                if (i === 1 || i === Row || j === 1 || j === Col || i==j || i-1==Col-j) {
+                    patternString += '* ';
+                } else {
+                    patternString += '  ';
+                }
+            }
+            patternString += '\n';
+        }
+    } else if(patternShape =='tic-tac-toe'){
+        for(let i = 1 ; i<=Row;i++){
+            for(let j = 1 ; j<=Col;j++){
+                if (i === 1 || i === Row || j === 1 || j === Col || j%7==0 || i%7==0) {
+                    patternString += '* ';
+                } else {
+                    patternString += '  ';
+                }
+            }
+            patternString += '\n';
+        }
+    }
+    
+    else if(patternShape=='diamond'){
+        for(let i= 1;i<=Row;i++){
+            for(let j= 1;j<=i;j++){
+                patternString +='*';
+            
+            }
+            patternString +='\n';
+        }
+        for(let i= 1;i<=Row;i++){
+            for(let j= Col;j>=i;j--){
+                patternString +='*';
+            
+            }
+            patternString +='\n';
+        }
+    }else if(patternShape=='left-arrow'){
+      
+            for(let i=1;i<=Row;i++){
+                for(let j=Col;j>=i;j--){
+                        patternString +='*';
+                }
+                patternString +='\n';
+            }
+            for(let i=1;i<=Row;i++){
+                for(let j=1;j<=i;j++){
+                        patternString +='*';
+                }
+                patternString +='\n';
+            }
+    }
+    else if(patternShape=='right-arrow'){
+        
+      
+        for(let i=1;i<=Row;i++){
+            for (let space = 2; space <= i; space++) {
+                patternString += ' ';
+            }
+            for(let j=Col;j>=i;j--){
+                    patternString +='*';
+            }
+            patternString +='\n';
+        }
+        for(let i=2;i<=Row;i++){
+            for (let space = 1; space <=Row-i; space++) {
+                patternString += ' ';
+            }
+            for(let j=1;j<=i;j++){
+                    patternString +='*';
+            }
+            patternString +='\n';
+        }
+}
+}
+
+return patternString;
+}
+let output = PatternMaker('star','octagon-square',13,13)
+console.log(output)
